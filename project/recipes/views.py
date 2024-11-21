61d81db9d1abcbc5fa7a0a55eb923e5e5a7f5ba8
msantos-lab <msantosdev.lab@gmail.com>
2024-11-21 09:42:53 +0100

    Configuration Recipe Details

--- project/recipes/views.py
+++ project/recipes/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'recipes/pages/index.html')
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe.html')
--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from project.recipes.views import index, recipe
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
+    path('recipes/<int:id>/', recipe),
 ]
