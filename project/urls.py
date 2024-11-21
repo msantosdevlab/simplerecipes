8dffb755b4ddbc1349ba64a1cd171b7132a4d6e3
msantos-lab <msantosdev.lab@gmail.com>
2024-11-21 15:20:26 +0100

    Unique name in URLs and app_name for Namespace

--- project/urls.py
+++ project/urls.py
@@ -15,12 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from recipes.views import index, recipe
-
+from django.urls import path, include 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
-    path('recipes/<int:id>/', recipe),
+    path('', include('recipes.urls')), 
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 # Create your views here.
 def index(request):
-    return render(request, 'recipes/pages/index.html')
+    return render(request, 'recipes/pages/index.html', context={'recipes': [make_recipe() for _ in range(10)], 'categories':[make_recipe() for _ in range(6)]})
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html')
+    return render(request, 'recipes/pages/recipe-view.html', context={'recipe': make_recipe()})
