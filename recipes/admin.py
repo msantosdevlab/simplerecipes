48e67a2c5bd2fe4f95e516a2247d185f8bee9aca
msantos-lab <msantosdev.lab@gmail.com>
2024-11-23 11:29:52 +0100

    Add Recipes and Category in Admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Category, Recipe
+
+#admin.site.site_header = "Painel Administrativo"
+#admin.site.site_title = "Administração do Projeto"
+#admin.site.index_title = "Bem-vindo ao painel"
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
