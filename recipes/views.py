d911c836d28b4a0025e05de245d1c172ff6eaa14
msantos-lab <msantosdev.lab@gmail.com>
2024-11-25 13:15:25 +0100

    Display categories from the database

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
 from django.templatetags.static import static
-from .models import Recipe
+from .models import Recipe, Category
 
 # Create your views here.
 def index(request):
     recipes = Recipe.objects.all().order_by('-id')
+    categories = Category.objects.all().order_by('-id')
     for recipe in recipes:
         recipe.cover_url = recipe.cover.url if recipe.cover else static('recipes/images/default-image.png')
-    return render(request, 'recipes/pages/index.html', context={'recipes': recipes})
+    return render(request, 'recipes/pages/index.html', context={'recipes': recipes, 'categories': categories})
 
 def recipe(request, id):
     recipe = Recipe.objects.get(id=id)
