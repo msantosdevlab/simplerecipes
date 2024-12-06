ce4c99dc148ab9458e171fd46ec27a200faa6799
msantos-lab <msantosdev.lab@gmail.com>
2024-12-06 12:09:39 +0100

    Test Recipes code 200 and correct template

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -14,4 +14,12 @@ class RecipeViewsTest(TestCase):
 
     def test_recipe_view_function(self):
         view = resolve(reverse('recipes:recipe', kwargs={'id' : 1}))
-        self.assertIs(view.func, views.recipe, msg="corretinho")
+        self.assertIs(view.func, views.recipe)
+
+    def test_recipe_home_view_returns_status_code_200_ok(self):
+        response = self.client.get(reverse('recipes:index'))
+        self.assertEqual(response.status_code, 200, msg=f"O código recebido foi {response.status_code}, mas esperávamos 200.")
+
+    def test_recipe_home_view_loads_correct_template(self):
+        response = self.client.get(reverse('recipes:index'))
+        self.assertTemplateUsed(response, 'recipes/pages/index.html')
